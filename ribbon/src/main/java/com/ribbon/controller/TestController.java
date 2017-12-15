35d6ec240a4acae1a5b3eb69ae51586e59f4a72d
fhb <fhb@example.com>
2017-12-15 08:52:11 +0800

    测试ribbon负载均衡

--- ribbon/src/main/java/com/ribbon/controller/TestController.java
+++ ribbon/src/main/java/com/ribbon/controller/TestController.java
@@ -3,6 +3,7 @@ package com.ribbon.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,7 +14,8 @@ public class TestController {
     private RestTemplate restTemplate;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String info() {
-        return restTemplate.getForEntity("http://SERVICE1/hello", String.class).getBody();
+    public String info(@RequestParam String name) {
+    	
+        return restTemplate.getForEntity("http://SERVICE1/hello?name="+name, String.class).getBody();
     }
 }
